f885d4a1ebf5ca886ff2345e73cb3a7f22e5896b
lythanhhai <lythanhhait@gmail.com>
2022-06-18 23:13:27 +0700

    commit 18/06/2022

---
+++ app/src/main/java/com/midterm/english_app/domain/repository/WordBaseRepository.kt
@@ -0,0 +1,23 @@
+package com.midterm.english_app.domain.repository
+
+import com.javalon.englishwhiz.data.local.entity.BookmarkEntity
+import com.javalon.englishwhiz.data.local.entity.DictionaryEntity
+import com.javalon.englishwhiz.data.local.entity.HistoryEntity
+import com.javalon.englishwhiz.util.Resource
+import kotlinx.coroutines.flow.Flow
+
+interface WordBaseRepository {
+
+    suspend fun insertBookmark(bookmarkEntity: BookmarkEntity)
+
+    suspend fun insertHistory(historyEntity: HistoryEntity)
+
+    suspend fun getAllBookmark(): Flow<List<BookmarkEntity>>
+
+    suspend fun getAllHistory(): Flow<List<HistoryEntity>>
+
+    suspend fun deleteBookmark(bookmarkEntity: BookmarkEntity)
+
+    suspend fun deleteHistory(historyEntity: HistoryEntity)
+
+}
